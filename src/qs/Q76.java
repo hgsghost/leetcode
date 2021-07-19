c47d9c28e10791c66a2c56bb923d35503c56bd63
Administrator <hgsghost@gmail.com>
2021-07-19 22:58:08 +0800

    高效滑动窗口

--- src/qs/Q76.java
+++ src/qs/Q76.java
@@ -36,97 +36,41 @@ s 和 t 由英文字母组成
         return null;
     }
     public String minWindow(String s, String t) {
-        int left=0;
-        int right=0;
         if(s.length()<t.length()){
             return "";
         }
+        int left=0;
+        int right=0;
+        int count=0;
         int start=0;
         int len=-1;
-        int valid=0;
-        String[] sp=t.split("");
-        Map<String,Integer> need=new HashMap<>();
-        Map<String,Integer> window=new HashMap<>();
-        for(int i=0;i<sp.length;i++){
-            String sub=sp[i];
-            if(need.containsKey(sub)){
-                need.put(sub,need.get(sub)+1);
-            }else{
-                need.put(sub, 1);
-            }
+        int[] need=new int[128];
+        int[] window=new int[128];
+        for(char c:t.toCharArray()){
+            need[c]++;
         }
         while(right<s.length()){
-            String sub=s.substring(right,right+1);
-            if(need.containsKey(sub)){
-                int a=0;
-                if(window.containsKey(sub)){
-                    window.put(sub, a=(window.get(sub)+1));
-                }else{
-                    window.put(sub, a=1);
-                }
-                if(a<=need.get(sub)){
-                    valid++;
-                }
+            char c=s.charAt(right++);
+            if(need[c]>0&&window[c]<need[c]){
+                count++;
             }
-            right++;
-
-            while(valid==t.length()&&((need.containsKey(s.substring(left,left+1))&&window.get(s.substring(left,left+1))>need.get(s.substring(left,left+1)))||!need.containsKey(s.substring(left,left+1)))){
-                if(need.containsKey(s.substring(left,left+1))){
-
-                    window.put(s.substring(left,left+1), window.get(s.substring(left,left+1))-1);
-                }
-                left++;
-            }
-            if(valid==t.length()){
+            window[c]++;
+            while(count==t.length()){
                 int newlen=right-left;
                 if(len==-1||newlen<len){
                     start=left;
                     len=newlen;
                 }
-            }
-        }
-        return len==-1?"":s.substring(start, start+len);
-    }
-    boolean getContains(Map<String,Integer> need,Map<String,Integer> window){
-        if(need.size()>window.size()){
-            return false;
-        }else{
-            return need.entrySet().stream().allMatch(i->i.getValue()<=window.get(i.getKey()));
-        }
+                c=s.charAt(left++);
 
-    };
-    /*public String minWindow(String s, String t) {
-        int sLen = s.length(), tLen = t.length();
-        // hash 用来统计每个字符出现的次数
-        // 在 t 中出现减一 ，在 s 中出现加一
-        // 当 hash 中所有字符出现的次数都非负，说明匹配上了
-        int[] hash = new int[128];
-        for (char ch : t.toCharArray()) {
-            hash[ch]--;
-        }
-        String ans = "";
-        // left: ans 的左端位置，
-        // right： ans 的右端位置
-        // cnt：已经匹配上的字符数
-        for (int left = 0, right = 0, cnt = 0; right < sLen; right++) {
-            // 如果当前字符可以匹配到 t 中某个尚未匹配的字符，就将 cnt 加一
-            if ((++hash[s.charAt(right)]) <= 0) {
-                cnt++;
-            }
-            // 如果 t 中所有字符均已匹配上，那么试图缩紧左边界
-            while (cnt == tLen && hash[s.charAt(left)] > 0) {
-                hash[s.charAt(left++)]--;
-            }
-            // 如果 t 中所有字符均已匹配上，尝试更新 ans
-            if (cnt == tLen) {
-                // 只在 ans 为空字符串或者 ans 长度大于当前长度时更新
-                if (ans.equals("") || ans.length() > right - left + 1) {
-                    ans = s.substring(left, right + 1);
+                if(need[c]>0&&need[c]==window[c]){
+                    count--;
                 }
+                window[c]--;
             }
         }
-        return ans;
-    }*/
+        return len==-1?"":s.substring(start,start+len);
+    }
 
 
 }
